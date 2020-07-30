2e430032f2a35ec14dba5e1e7264bf625dfa7611
Ahmed <ahmed2abd0l6ziz@gmail.com>
2020-07-30 18:08:49 +0200

    first commit and create home page with html and css

---
+++ index.php
@@ -0,0 +1,211 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>bookshelf</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="https://fonts.googleapis.com/css2?family=Cairo:wght@600&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/bootstrap-rtl.css">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<!-- start navbar -->
+		<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+			<!-- <div class="container"> -->
+				<a href="index.php" class="navbar-brand">BookShelf</a>
+				<button class="navbar-toggler" data-toggle="collapse" data-target="#menu">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="menu">
+					<ul class="navbar-nav ml-auto">
+						<li class="nav-item">
+							<a href="#" class="nav-link">الرئيسية</a>
+						</li>
+						<li class="nav-item">
+							<a href="#" class="nav-link">تصنيفات</a>
+						</li>
+						<li class="nav-item">
+							<a href="#" class="nav-link">المؤلفون</a>
+						</li>
+						<li class="nav-item">
+							<a href="#" class="nav-link">تواصل معنا</a>
+						</li>
+					</ul>
+				</div>
+		</nav>
+	<!-- end navbar -->
+	<!-- start content-->
+				<div class="row">
+				<div class="col-md-3" id="side-area">
+					<div class="container-fluid">
+						<h4 >تصنيفات الكتب</h4>
+						<ul>
+							<li>
+								<a href="catigories.php">
+									<span><i class="fas fa-tags"></i></span>
+									<span>روايات عربية</span>
+								</a>
+							</li>
+							<li>
+								<a href="catigories.php">
+									<span><i class="fas fa-tags"></i></span>
+									<span>روايات عالمية</span>
+								</a>
+							</li>
+							<li>
+								<a href="catigories.php">
+									<span><i class="fas fa-tags"></i></span>
+									<span>شعر عربي</span>
+								</a>
+							</li>
+							<li>
+								<a href="catigories.php">
+									<span><i class="fas fa-tags"></i></span>
+									<span>شعر عالمي</span>
+								</a>
+							</li>
+							<li>
+								<a href="catigories.php">
+									<span><i class="fas fa-tags"></i></span>
+									<span>تاريخ و جغرافيا</span>
+								</a>
+							</li>
+							<li>
+								<a href="catigories.php">
+									<span><i class="fas fa-tags"></i></span>
+									<span>كتب عالمية مترجمة</span>
+								</a>
+							</li>
+							<li>
+								<a href="catigories.php">
+									<span><i class="fas fa-tags"></i></span>
+									<span>علم نفس و اجتماع</span>
+								</a>
+							</li>
+							<li>
+								<a href="catigories.php">
+									<span><i class="fas fa-tags"></i></span>
+									<span>فلسفة ومنطق</span>
+								</a>
+							</li>
+							<li>
+								<a href="catigories.php">
+									<span><i class="fas fa-tags"></i></span>
+									<span>كتب علمية و تقنية</span>
+								</a>
+							</li>
+							<li>
+								<a href="catigories.php">
+									<span><i class="fas fa-tags"></i></span>
+									<span>كتب الطب و الاحياء</span>
+								</a>
+							</li>
+							<li>
+								<a href="catigories.php">
+									<span><i class="fas fa-tags"></i></span>
+									<span>كتب متنوعة</span>
+								</a>
+							</li>
+							<li>
+								<a href="catigories.php">
+									<span><i class="fas fa-tags"></i></span>
+									<span>كتب اسالمية</span>
+								</a>
+							</li>
+							<li>
+								<a href="catigories.php">
+									<span><i class="fas fa-tags"></i></span>
+									<span>تصوف</span>
+								</a>
+							</li>
+							<li>
+								<a href="catigories.php">
+									<span><i class="fas fa-tags"></i></span>
+									<span>كتب سياسية</span>
+								</a>
+							</li>
+							<li>
+								<a href="catigories.php">
+									<span><i class="fas fa-tags"></i></span>
+									<span>إقتصاد</span>
+								</a>
+							</li>
+						</ul>	
+					</div>
+			</div>
+				<div class="col-md-9">
+					
+						<div class="row" id="books">
+							<div class="col-sm-3" id="post">
+								<img src="img/1.jpg">
+								<h4>عنوان الكتاب</h4>
+								<h6>اسم المؤلف</h6>
+								<a href="#">تحميل الكتاب</a>
+							</div>
+							<div class="col-sm-3" id="post">
+								<img src="img/1.jpg">
+								<h4>عنوان الكتاب</h4>
+								<h6>اسم المؤلف</h6>
+								<a href="#">تحميل الكتاب</a>
+							</div>
+							<div class="col-sm-3" id="post">
+								<img src="img/1.jpg">
+								<h4>عنوان الكتاب</h4>
+								<h6>اسم المؤلف</h6>
+								<a href="#">تحميل الكتاب</a>
+							</div>
+							<div class="col-sm-3" id="post">
+								<img src="img/1.jpg">
+								<h4>عنوان الكتاب</h4>
+								<h6>اسم المؤلف</h6>
+								<a href="#">تحميل الكتاب</a>
+							</div>
+							<div class="col-sm-3" id="post">
+								<img src="img/1.jpg">
+								<h4>عنوان الكتاب</h4>
+								<h6>اسم المؤلف</h6>
+								<a href="#">تحميل الكتاب</a>
+							</div>
+							<div class="col-sm-3" id="post">
+								<img src="img/1.jpg">
+								<h4>عنوان الكتاب</h4>
+								<h6>اسم المؤلف</h6>
+								<a href="#">تحميل الكتاب</a>
+							</div>
+							<div class="col-sm-3" id="post">
+								<img src="img/1.jpg">
+								<h4>عنوان الكتاب</h4>
+								<h6>اسم المؤلف</h6>
+								<a href="#">تحميل الكتاب</a>
+							</div>
+							<div class="col-sm-3" id="post">
+								<img src="img/1.jpg">
+								<h4>عنوان الكتاب</h4>
+								<h6>اسم المؤلف</h6>
+								<a href="#">تحميل الكتاب</a>
+							</div>
+
+							<div class="col-sm-3" id="post">
+								<img src="img/1.jpg">
+								<h4>عنوان الكتاب</h4>
+								<h6>اسم المؤلف</h6>
+								<a href="#">تحميل الكتاب</a>
+							</div>
+						</div>
+					
+				</div>
+			</div>
+		
+	<!-- end content -->
+	<!-- start footer -->
+		<footer>
+			<a href="#">جميع الحقوق محفوظة 	&copy; 2020</a>
+		</footer>
+	<!--end footer -->
+
+	
+	<script src="js/jquery.min.js"></script>
+	<script src="https://kit.fontawesome.com/03757ac844.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
